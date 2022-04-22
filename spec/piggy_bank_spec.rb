74a245ce7331eed6b3c62cd3aed6c05571396d8b
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-22 11:44:12 +0100

    pair swap

---
+++ spec/piggy_bank_spec.rb
@@ -0,0 +1,19 @@
+require 'piggy_bank'
+
+describe PiggyBank do
+
+  it 'can put coins '
+  
+end
+
+# As a user,
+# So that I can save money
+# I can put coins in my piggy bank
+
+
+
+# arrange, act, assert
+    # arrange - what instances, classes etc, 
+      #do you need to carry out the test?
+    # Act - what code do you need to run
+    # Assert - comparing expected behaviour with actual behavious
