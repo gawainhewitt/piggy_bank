becab854c4427e4ae22a8399a716027836f62a4b
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-22 12:29:39 +0100

    complete

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,3 +1,18 @@
 class PiggyBank 
-  
+
+  def initialize
+    @coin = []
+  end
+    
+  def put_coins_in(coin)
+    @coin << coin
+  end
+
+  def shake
+    "cling"
+  end
+
+  def break
+    @coin
+  end
 end
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -3,21 +3,40 @@ require 'piggy_bank'
 describe PiggyBank do
 
   it 'can put coins in the piggy bank' do
-    # arrange
     piggybank = PiggyBank.new
-    # act
     coin = '50p'
-    # assert
     expect { piggybank.put_coins_in(coin) }.not_to raise_error
+  end
 
+  it 'if money is in the piggybank returns "cling" when shaken' do
+    #arrange
+    piggybank = PiggyBank.new
+    #act
+    coin = '50p'
+    piggybank.put_coins_in(coin)
+    #assert
+    expect(piggybank.shake).to eq 'cling'
   end
   
+  it 'returns coins when broken' do
+    #arrange
+    piggybank = PiggyBank.new
+    #act
+    piggybank.put_coins_in('50p')
+    piggybank.put_coins_in('30p')
+    piggybank.put_coins_in('70p')
+    piggybank.put_coins_in('10p')
+
+    #assert
+    expect(piggybank.break).to eq ['50p', '30p', '70p', '10p']
+  end
+
+
 end
 
 # As a user,
-# So that I can save money
-# I can put coins in my piggy bank
-
+# So I can enjoy my hard earned savings
+# I can break my piggy bank and get back the number or coins that I had put in
 
 
 # arrange, act, assert
